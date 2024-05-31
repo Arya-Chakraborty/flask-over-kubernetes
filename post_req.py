8d59eeab59cf799cf9b12db5c8de325b209bc37e
Arya-Chakraborty <76962037+Arya-Chakraborty@users.noreply.github.com>
2024-05-31 07:19:45 +0530

    Update post_req.py

--- post_req.py
+++ post_req.py
@@ -1,6 +1,6 @@
 import requests
 dictToSend = {'question':'what is your name?'}
-res = requests.post('http://localhost:5000/', json=dictToSend, timeout=10)
+res = requests.post('http://127.0.0.1:41041/', json=dictToSend, timeout=10)
 # print('response from server:',res.text)
 dictFromServer = res.json()
 print('response from server:',dictFromServer["name"])
