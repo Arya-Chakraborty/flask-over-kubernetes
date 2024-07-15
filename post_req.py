3d4a651c436e4b4b189b1a440f30ca9ca6fae49c
Arya-Chakraborty <aryastlawrence@gmail.com>
2024-07-15 20:22:06 +0530

    test

--- post_req.py
+++ post_req.py
@@ -1,6 +1,6 @@
 import requests
-dictToSend = {'question':'what is your name?'}
-res = requests.post('http://localhost:5000/', json=dictToSend, timeout=10)
+dict_to_send = {'question':'what is your name?'}
+res = requests.post('http://127.0.0.1:41041/', json=dict_to_send, timeout=10)
 # print('response from server:',res.text)
-dictFromServer = res.json()
-print('response from server:',dictFromServer["name"])
+dict_from_server = res.json()
+print('response from server:',dict_from_server["name"])
